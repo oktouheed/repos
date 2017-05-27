d619349ccdd381ebe8c48a4cbd4a96c4cadf3f7c
oktouheed <oktouheed@gmail.com>
2017-05-27 12:34:40 +0600

    First Comment

---
+++ my-github-repos.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: My Github Repos
+ * Description: Display user repos in widget
+ * Version: 1.0
+ * Author: Touheed Hasan
+**/
+
+// Exit if Accessed Directly
+if(!defined('ABSPATH')) {
+	exit;
+}
+
+// Include Scripts
+require_once(plugin_dir_path(__FILE__) . '/includes/my-github-repos-scripts.php');
+
+// Include class
+require_once(plugin_dir_path(__FILE__) . '/includes/my-github-repos-class.php');
+
+// Register widget
+function mgr_register_widget() {
+	register_widget('WP_My_Github_Repos');
+}
+
+add_action('widgets_init', 'mgr_register_widget');
